4068f4302b36bcf41b41b2e780e84e6f8c554f77
Mike <5188083@gmail.com>
2021-08-22 18:57:13 +0200

    full outlets stsrt

--- SlidersShow/ViewController.swift
+++ SlidersShow/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redLabelStatic: UILabel!
+    @IBOutlet var redLabelValue: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    
+    @IBOutlet var greenLabelStatic: UILabel!
+    @IBOutlet var greenLabelValue: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var yellowLabelStatic: UILabel!
+    @IBOutlet var yellowLabelValue: UILabel!
+    @IBOutlet var yellowSlider: UISlider!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
